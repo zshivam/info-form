11b4540c241084b91c8d722198419cd5dcd0af7e
shivam sahani <zshivam19@gmail.com>
2025-07-19 14:51:07 +0530

    Initial commit with frontend and backend

---
+++ backend/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+print("DB_USER:", os.getenv("DB_USER"))
+print("DB_PORT:", os.getenv("DB_PORT"))
+
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
