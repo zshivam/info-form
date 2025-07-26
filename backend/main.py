1ee8f5fde933dc5cdfb5635aef0341ec136618b5
shivam sahani <zshivam19@gmail.com>
2025-07-26 18:31:10 +0530

    corrections in main.py

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # Allow frontend to call backend
 app.add_middleware(
     CORSMiddleware,
-   allow_origins=["*"],
+   allow_origins=["https://info-form-9q9n.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
